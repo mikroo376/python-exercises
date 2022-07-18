f451d9265ee6ef8eda6a09386afe40814701f2c1
Michal Nowakowski <mikro.michal@gmail.com>
2022-07-18 16:12:04 +0200

    Klasy w pythonie

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-lista = ['elo', 1, 2, 3]
-print(lista)
-lista.remove(lista[0])
+class Car():
+    def __init__(self, productionYear, brand):
+        self.productionYear = productionYear
+        self.model = brand
+    def startEngine(self):
+        return print("Starting engine...")
+    def StopEngine(self):
+        return print("Stopping engine...")
 
-print(lista)
 
-test = [1,2,3]
+class Honda(Car):
+    def __init__(self, model, productionYear, brand):
+        self.model = model
+        super().__init__(productionYear, brand)
+
+
+
+my_car = Car(2010, "Honda")
+my_car2 = Car(2018, "BMW")
+
+honda = Honda("Civic", 1995, "Honda")
+
+
+
+print(honda.productionYear)
