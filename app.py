22b02f096dc8849f1b2c52d26acc6f363ac67fcd
Michal Nowakowski <mikro.michal@gmail.com>
2022-07-19 16:55:49 +0200

    Calculator in functions

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
-operation = 0
-result = 0
-steps = []
-prevOperation = ""
+def init():
+    operation = 0
+    steps = []
+    while operation != 5:
+        number = int(input("Type number: "))
+        operation = int(input("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Result\nOperation: "))
+        steps.append(number)
+        if operation > 5:
+            print("Please choose correct operation")
+            break
+        elif operation == 1:
+            steps.append("+")
+        elif operation == 2:
+            steps.append("-")
+        elif operation == 3:
+            steps.append("*")
+        elif operation == 4:
+           steps.append("/")
+    else:
+
+      return calculate(steps)
+        
 
 
-
-
-                                                
-while operation != 5:
-    number = int(input("Type number: "))
-    operation = int(input("1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Result\nOperation: "))
-    steps.append(number)
-    if operation > 5:
-        print("Please choose correct operation")
-        break
-    elif operation == 1:
-        steps.append("+")
-    elif operation == 2:
-        steps.append("-")
-    elif operation == 3:
-        steps.append("*")
-    elif operation == 4:
-        steps.append("/")
-else:
-
+def calculate(steps):
+    prevOperation = ""
+    result = 0
     for element in steps:
-        # print(prevOperation)
         if prevOperation == "":
             result += element
             prevOperation = "x"
         elif type(element) == type(""):
             prevOperation = element
-            
+                
         elif type(element) == type(0) and prevOperation == "+":
             result += element
         elif type(element) == type(0) and prevOperation == "-":
@@ -40,6 +40,8 @@ else:
             result *= element
         elif type(element) == type(0) and prevOperation == "/":
             result /= element
-   
-    print(f"{'#'*100}\nRESULT = {steps} = {result}\n{'#'*100}")
 
+    return print(f"{'#'*100}\nRESULT = {steps} = {result}\n{'#'*100}")
+                                                
+
+init()
